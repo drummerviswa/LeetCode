91b212d561ee0200ade3b095c9d0765097a68027
Viswanathan <drummerviswa@gmail.com>
2024-02-18 12:46:17 +0530

    Day 2

--- 2addTwoNumbers.py
+++ 2addTwoNumbers.py
@@ -1,16 +1,33 @@
-# Definition for singly-linked list.
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        suml1=0
-        suml2=0
-        for a in l1:
-            suml1+=a
-        print(suml1)
-        for b in l2:
-            suml2+=b
-        print(suml2)
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
 
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
+
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
+l1 = ListNode(234)
+l2 = ListNode(564)
 a = Solution()
-l1 = [5,6,4]
-l2 = [2,4,3]
-a.addTwoNumbers(l1,l2)
+result = a.addTwoNumbers(l1,l2)
+print(result.val)
